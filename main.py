44eb322aaeb77c58d01e2ebdfd04acf9cec580b0
bryanbuiles <1482@holbertonschool.com>
2020-10-10 15:22:19 -0500

    classes integrated with postgrepsql

---
+++ main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+ Test cities access from a state
+"""
+from models.user import User
+from models.collar import Collar
+from models.pet import Pet
+
+"""
+ Objects creations
+"""
+user_1 = User(nickname="bryan", password="12345",
+              email="1482@holbertonschool.com")
+user_1.save()
+
+pet_1 = Pet(user_id=user_1.id, name="Napa", race="labrador")
+pet_1.save()
+
+collar_1 = Collar(user_id=user_1.id, pet_id=pet_1.id)
+collar_1.save()
+"""
+ Verification
+"""
+print("Verificacion exitosa")
--- models/db_storage.py
+++ models/db_storage.py
@@ -3,24 +3,63 @@
 Contains the class DBStorage
 """
 
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from models.collar import Collar
 from models.pet import Pet
 from models.user import User
 from os import getenv
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 classes = {"User": User, "Pet": Pet, "Collar": Collar}
 
 
 class DBStorage:
-    """interaacts with the MySQL database"""
+    """interaacts with the SQL database"""
     __engine = None
     __session = None
 
     def __init__(self):
         """Instantiate a DBStorage object"""
-        POSTGREP_USER = getenv('HBNB_MYSQL_USER')
-        POSTGREP_PWD = getenv('HBNB_MYSQL_PWD')
-        POSTGREP_HOST = getenv('HBNB_MYSQL_HOST')
-        POSTGREP_DB = getenv('HBNB_MYSQL_DB')
-        POSTGREP_ENV = getenv('HBNB_ENV')
+        POSTGREP_USER = getenv('POSTGREP_USER')
+        POSTGREP_PWD = getenv('POSTGREP_PWD')
+        POSTGREP_HOST = getenv('POSTGREP_HOST')
+        POSTGREP_DB = getenv('POSTGREP_DB')
+        self.__engine = create_engine("postgresql://{}:{}@{}/{}".format(
+            POSTGREP_USER, POSTGREP_PWD, POSTGREP_HOST, POSTGREP_DB))
+
+    def all(self, cls=None):
+        """query on the current database session"""
+        new_dict = {}
+        for clss in classes:
+            if cls is None or cls is classes[clss] or cls is clss:
+                objs = self.__session.query(classes[clss]).all()
+                for obj in objs:
+                    key = obj.__class__.__name__ + '.' + obj.id
+                    new_dict[key] = obj
+        return (new_dict)
+
+    def new(self, obj):
+        """add the object to the current database session"""
+        self.__session.add(obj)
+
+    def save(self):
+        """commit all changes of the current database session"""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """delete from the current database session obj if not None"""
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """reloads data from the database"""
+        Base.metadata.create_all(self.__engine)
+        sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(sess_factory)
+        self.__session = Session
+
+    def close(self):
+        """call remove() method on the private session attribute"""
+        self.__session.remove()
