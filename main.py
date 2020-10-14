2823614b72e1c628409a91ce43ba40b6e12d3906
bryanbuiles <1482@holbertonschool.com>
2020-10-14 11:29:41 -0500

    cambios server

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 """
  Test cities access from a state
 """
+from models import storage
 from models.user import User
 from models.collar import Collar
 from models.pet import Pet
@@ -15,6 +16,9 @@ user_1.save()
 
 pet_1 = Pet(user_id=user_1.id, name="Napa", race="labrador")
 pet_1.save()
+
+pet_2 = Pet(user_id=user_1.id, name="Napa", race="labrador")
+pet_2.save()
 
 collar_1 = Collar(user_id=user_1.id, pet_id=pet_1.id)
 collar_1.save()
