d01eb48d9125862bff938af3ff3be85da5f78a15
erikaosgue <1476@holbertonschool.com>
2020-10-20 17:53:22 +0000

    Update main information

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from models import storage
 from models.user import User
 from models.collar import Collar
 from models.pet import Pet
+from datetime import date
 
 """
  Objects creations
@@ -16,7 +17,8 @@ user_1 = User(nickname="bryan",
               email="1482@holbertonschool.com", auth_id="a0t14527")
 user_1.save()
 
-pet_1 = Pet(user_id=user_1.id, name="Napa", race="labrador")
+pet_1 = Pet(user_id=user_1.id, name="Napa",
+            race="labrador", birthday=date(2020, 9, 26), color="Yellow", specie="Perro", description="Cute new born")
 pet_1.save()
 
 pet_2 = Pet(user_id=user_1.id, name="firulais", race="labrador")
