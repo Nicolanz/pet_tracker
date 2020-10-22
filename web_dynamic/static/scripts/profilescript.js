61c84e55844e4dbfe1bbbaf81a8e4e2675d26f4a
bryanbuiles <1482@holbertonschool.com>
2020-10-22 15:41:26 -0500

    fix conflicts

--- web_dynamic/static/scripts/profilescript.js
+++ web_dynamic/static/scripts/profilescript.js
@@ -3,18 +3,17 @@ const alert = window.alert;
 $(document).ready(function () {
   const user_id = document.getElementById('sub').value;
   $.ajax('http://localhost:5000/api/v1/users/' + user_id, {
-    type: 'GET'
+    type: 'GET',
   }).done(function (data) {
     $('#username').append(data.nickname);
   });
 
   $.ajax('http://localhost:5000/api/v1/users/' + user_id + '/pets', {
-    type: 'GET'
+    type: 'GET',
   }).done(function (data) {
     for (const pets of data) {
       let new_date = new Date(pets.birthday);
       let birthday = new_date.toISOString().split('T')[0];
-      console.log(pets.id)
       $('.pet').prepend(
         `<div class="pet_target col-7 container-fluid d-flex align-items-center justify-content-center flex-row flex-wrap bg-light rounded">
           <div class="foto col-xl-4 col-lg-4 col-md-4 col-sm-6 col-12">
@@ -42,14 +41,15 @@ $(document).ready(function () {
               <li class="list-group-item"><b>Cumpleaños: </b> ${birthday} </li>
               <li class="list-group-item"><b>Sex: </b> ${pets.sex} </li>
             </ul>
-            <br><br> 
+            <br><br>
             <form action="/add_collar">
               <div class="col-12 d-flex flex-row flex-nowrap justify-content-center">
                 <input type="text" class="form-control col-9 pet-id-${pets.id}" name="collar_id" placeholder="Collar Id">
-                <button type="button" class="btn btn-default ml-2 button-add-collar" data-pet-id="${pets.id}" data-user-id="${user_id}">ADD</button>
+                <button type="button" class="btn btn-default ml-2 button-add-collar" data-pet-id="${pets.id}" data-user-id="${user_id}">Add</button>
+                <button type="button" class="btn btn-default ml-2 button-delete-collar" data-pet-id="${pets.id}" data-user-id="${user_id}">Delete</button>
               </div>
-            </form>  
-          </div> 
+            </form>
+          </div>
 
           <div class="iconos d-flex justify-content-center col-xl-3 col-lg-3 col-6">
             <a href="/pet_location?pet-id=${pets.id}" class="d-flex align-items-center" id="img3">
@@ -63,7 +63,7 @@ $(document).ready(function () {
                 <path fill-rule="evenodd" d="M8.932.727c-.243-.97-1.62-.97-1.864 0l-.071.286a.96.96 0 0 1-1.622.434l-.205-.211c-.695-.719-1.888-.03-1.613.931l.08.284a.96.96 0 0 1-1.186 1.187l-.284-.081c-.96-.275-1.65.918-.931 1.613l.211.205a.96.96 0 0 1-.434 1.622l-.286.071c-.97.243-.97 1.62 0 1.864l.286.071a.96.96 0 0 1 .434 1.622l-.211.205c-.719.695-.03 1.888.931 1.613l.284-.08a.96.96 0 0 1 1.187 1.187l-.081.283c-.275.96.918 1.65 1.613.931l.205-.211a.96.96 0 0 1 1.622.434l.071.286c.243.97 1.62.97 1.864 0l.071-.286a.96.96 0 0 1 1.622-.434l.205.211c.695.719 1.888.03 1.613-.931l-.08-.284a.96.96 0 0 1 1.187-1.187l.283.081c.96.275 1.65-.918.931-1.613l-.211-.205a.96.96 0 0 1 .434-1.622l.286-.071c.97-.243.97-1.62 0-1.864l-.286-.071a.96.96 0 0 1-.434-1.622l.211-.205c.719-.695.03-1.888-.931-1.613l-.284.08a.96.96 0 0 1-1.187-1.186l.081-.284c.275-.96-.918-1.65-1.613-.931l-.205.211a.96.96 0 0 1-1.622-.434L8.932.727zM8 12.997a4.998 4.998 0 1 0 0-9.995 4.998 4.998 0 0 0 0 9.996z"/>
               </svg>
             </a>
-            
+
             <span class="d-flex align-items-center" id="img6" onclick="remove('${pets.id}')" >
               <svg width="100%" height="70%" viewBox="0 0 16 16" class="bi bi-trash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
@@ -83,87 +83,120 @@ $(document).ready(function () {
 
       requestApi(userId, petId, collarId);
     });
-  });
+    $('.button-delete-collar').click((buttonElement) => {
+      const petId1 = buttonElement.currentTarget.dataset.petId;
+      // const userId1 = buttonElement.currentTarget.dataset.userId1;
 
-  function requestApi (user_id, pet_id, collar_id) {
-  // Request to API of company to get the id of the collar
-    const url = 'https://jsonplaceholder.typicode.com/todos?id=' + collar_id;
-    $.ajax({
-      type: 'GET',
-      url: url,
-      dataType: 'text',
-      error: function (xhr, status, error) {
-        const err = JSON.parse(xhr.responseText);
-        alert(err.Message);
-      },
-      success: function (data) {
-        if (data === '[]') {
-          alert(' Id does not exist in the API! ');
-        } else {
-          alert(' Id exist :D in the IP! ');
-          verifyCollar(user_id, pet_id, collar_id);
-        }
-      }
-    });
-  }
+      // Get the value of collar
+      const inputElement = document.querySelector('.pet-id-' + petId1);
+      const collar_id = inputElement.value;
 
-  function verifyCollar (user_id, pet_id, collar_id) {
-    $.ajax('http://localhost:5000/api/v1/pets/' + pet_id + '/collars', {
-      type: 'GET'
-    }).done(data => {
-      if (data.status === 'NO EXIST') {
-        /* La mascota no debe tener asociado ningun collar ni el collar debe existir */
-        $.ajax('http://localhost:5000/api/v1/collars/' + collar_id, {
-          type: 'GET',
-          success: function (data) {
-            if (data.status === 'NO EXIST') {
-              CreateCollar(user_id, pet_id, collar_id);
-              alert(' Collar creado satisfactoriamente! ');
+      const result = confirm('Desea eliminar el collar');
+      if (result) {
+        axios({
+          method: 'get',
+          url: 'http://localhost:5000/api/v1/pets/' + petId1 + '/collars',
+          responseType: 'json',
+        })
+          .then(function (response) {
+            console.log(response.data.status);
+            if (response.data.status === 'EXIST') {
+              axios({
+                method: 'delete',
+                url: 'http://localhost:5000/api/v1/collars/' + collar_id,
+                responseType: 'json',
+              }).then(function (response) {
+                alert('el collar ha sido eliminado');
+              });
             } else {
-              alert(' Collar ya existe ! ');
+              alert('el id del collar no esta asignado al usuario');
             }
-          }
-        });
-      } else {
-        alert(' Pet ya tiene collar asignado ! ');
+          })
+          .catch(function (error) {
+            alert('Verifica si el id es correcto');
+          });
       }
     });
-  }
 
-  function CreateCollar (user_id, pet_id, collar_id) {
-    /* Create a new Collar */
-    $.ajax('http://localhost:5000/api/v1/collars', {
-      type: 'POST',
-      contentType: 'application/json',
-      dataType: 'json',
-      data: JSON.stringify({
-        user_id: user_id,
-        pet_id: pet_id,
-        numero_ref: collar_id
-      }),
-      error: function (xhr, status, error) {
-        const err = JSON.parse(xhr.responseText);
-        alert(err.Message);
-        alert('Error Eliminando Pet');
-      },
-      success: function () {
-        $('.pet-id-' + pet_id).text('Collar id: ' + collar_id);
-        alert(' Collar creado exitosamente ! ');
-      }
-    });
-  }
+    function requestApi(user_id, pet_id, collar_id) {
+      // Request to API of company to get the id of the collar
+      const url = 'https://jsonplaceholder.typicode.com/todos?id=' + collar_id;
+      $.ajax({
+        type: 'GET',
+        url: url,
+        dataType: 'text',
+        error: function (xhr, status, error) {
+          const err = JSON.parse(xhr.responseText);
+          alert(err.Message);
+        },
+        success: function (data) {
+          if (data === '[]') {
+            alert(' Id does not exist in the API! ');
+          } else {
+            alert(' Id exist :D in the IP! ');
+            verifyCollar(user_id, pet_id, collar_id);
+          }
+        },
+      });
+    }
+
+    function verifyCollar(user_id, pet_id, collar_id) {
+      $.ajax('http://localhost:5000/api/v1/pets/' + pet_id + '/collars', {
+        type: 'GET',
+      }).done((data) => {
+        if (data.status === 'NO EXIST') {
+          /* La mascota no debe tener asociado ningun collar ni el collar debe existir */
+          $.ajax('http://localhost:5000/api/v1/collars/' + collar_id, {
+            type: 'GET',
+            success: function (data) {
+              if (data.status === 'NO EXIST') {
+                CreateCollar(user_id, pet_id, collar_id);
+                alert(' Collar creado satisfactoriamente! ');
+              } else {
+                alert(' Collar ya existe ! ');
+              }
+            },
+          });
+        } else {
+          alert(' Pet ya tiene collar asignado ! ');
+        }
+      });
+    }
+
+    function CreateCollar(user_id, pet_id, collar_id) {
+      /* Create a new Collar */
+      $.ajax('http://localhost:5000/api/v1/collars', {
+        type: 'POST',
+        contentType: 'application/json',
+        dataType: 'json',
+        data: JSON.stringify({
+          user_id: user_id,
+          pet_id: pet_id,
+          numero_ref: collar_id,
+        }),
+        error: function (xhr, status, error) {
+          const err = JSON.parse(xhr.responseText);
+          alert(err.Message);
+          alert('Error Eliminando Pet');
+        },
+        success: function () {
+          $('.pet-id-' + pet_id).text('Collar id: ' + collar_id);
+          alert(' Collar creado exitosamente ! ');
+        },
+      });
+    }
+  });
 });
 // Function to remove pets onclick
-function remove (pet_id) {
+function remove(pet_id) {
   const result = confirm('¿Esta seguro que desea eliminar la mascota?');
-  console.log(typeof pet_id)
   if (result) {
     alert('borrando...');
     $.ajax('http://localhost:5000/api/v1/pets/' + pet_id, {
       type: 'DELETE',
       success: function () {
         alert('Pet eliminado correctamente!');
-      }
+      },
     });
     // $.ajax({
     //   url: 'http://localhost:5000/api/v1/pets/' + pet_id,
